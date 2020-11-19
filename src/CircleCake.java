a5b8248175d616bb28b81774fca8d375a6807f72
jj09679 <54376744+jj09679@users.noreply.github.com>
2020-11-19 17:34:01 -0500

    Add files via upload

---
+++ src/CircleCake.java
@@ -0,0 +1,10 @@
+public class CircleCake extends Cake{
+	 
+	public  CircleCake(){
+	        description = "Circle Cake";
+	    }
+	
+    public double cost() {
+	        return 10.00;
+	    }
+}
---
+++ src/SquareCake.java
@@ -0,0 +1,9 @@
+public class SquareCake extends Cake {
+	    public  SquareCake(){
+	        description = "Square Cake";
+	    }
+
+	    public double cost() {
+	        return 10.00;
+	    }
+}
